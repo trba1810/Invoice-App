2eaf278b1bfee838eccc5ff247fcb8255bad4a6b
trba1810 <trba8l@live.com>
2024-01-05 08:12:44 +0100

    add option to insert in bulk

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Invoice;
+use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use App\Filters\V1\InvoicesFilter;
 use App\Http\Controllers\Controller;
@@ -10,6 +11,7 @@ use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Resources\V1\InvoiceResource;
 use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Resources\V1\InvoiceCollection;
+use App\Http\Requests\V1\BulkStoreInvoiceRequest;
 
 class InvoiceController extends Controller
 {
@@ -43,6 +45,15 @@ class InvoiceController extends Controller
     public function store(StoreInvoiceRequest $request)
     {
         //
+    }
+
+    public function bulkStore(BulkStoreInvoiceRequest $request)
+    {
+        $bulk = collect($request->all())->map(function ($arr, $key) {
+            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+        });
+
+        Invoice::insert($bulk->toArray());
     }
 
     /**
---
+++ app/Http/Requests/V1/BulkStoreInvoiceRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class BulkStoreInvoiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            '*.customerId' => ['required', 'integer'],
+            '*.amount' => ['required', 'numeric'],
+            '*.status' => ['required', Rule::in(['B', 'P', 'V', 'b', 'p', 'v'])],
+            '*.billedDate' => ['required', 'date_format:Y-m-d H:i:s'],
+            '*.paidDate' => ['date_format:Y-m-d H:i:s', 'nullable'],
+
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $data = [];
+
+        foreach ($this->toArray() as $obj) {
+            $obj['customerId'] = $obj['customerId'] ?? null;
+            $obj['billedDate'] = $obj['billedDate'] ?? null;
+            $obj['paidDate'] = $obj['paidDate'] ?? null;
+
+            $data[] = $obj;
+        }
+
+        $this->merge($data);
+    }
+}
